86e25086180ae7c733439af6269ebbf9558511aa
jaewan <sjo9810@naver.com>
2025-04-08 17:04:32 +0900

    !HOTFIX: 보안취약점 제거 CBC 방식 -> GCM 방식으로 변경함

--- src/main/java/com/core/drm/crypto/domain/cipher/CipherWrapper.java
+++ src/main/java/com/core/drm/crypto/domain/cipher/CipherWrapper.java
@@ -2,20 +2,16 @@ package com.core.drm.crypto.domain.cipher;
 
 import org.bouncycastle.crypto.BlockCipher;
 import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.modes.GCMBlockCipher;
 import org.bouncycastle.crypto.paddings.BlockCipherPadding;
 import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
 
 /*
  * cipher 객체의 초기화 여부 포함하기 위해 상속함
  */
-public class CipherWrapper extends PaddedBufferedBlockCipher {
+public class CipherWrapper extends GCMBlockCipher {
 
     private boolean initFlag;
-
-    public CipherWrapper(BlockCipher blockCipher, BlockCipherPadding blockCipherPadding) {
-        super(blockCipher, blockCipherPadding);
-        blockCipher.reset();
-    }
 
     public CipherWrapper(BlockCipher blockCipher) {
         super(blockCipher);
