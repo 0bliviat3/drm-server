501fb8d6c1d1dd3de18aeb4c798a4385856924c5
jaewan <sjo9810@naver.com>
2025-04-21 17:53:13 +0900

    feat: DRM 예외에 대한 설정 추가
    - 고정 상수 리턴할수 있도록 인터페이스 설계

---
+++ src/main/java/com/core/drm/crypto/exception/DRMException.java
@@ -0,0 +1,9 @@
+package com.core.drm.crypto.exception;
+
+import com.core.drm.crypto.constant.errormessage.ResponseMessage;
+
+public interface DRMException {
+
+    ResponseMessage getResponseMessage();
+
+}
