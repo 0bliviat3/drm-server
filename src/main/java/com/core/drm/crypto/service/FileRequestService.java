e04f8fd6cc8915dbdcd1d88edfd013d24a78414c
jaewan <sjo9810@naver.com>
2025-05-08 16:56:12 +0900

    feat: FileRequestService 구현
    - save, findById 구현
    - 필요한 DB 연동로직 있을시 추가 구현

---
+++ src/main/java/com/core/drm/crypto/service/FileRequestService.java
@@ -0,0 +1,45 @@
+package com.core.drm.crypto.service;
+
+import com.core.drm.crypto.constant.CipherType;
+import com.core.drm.crypto.domain.entity.FileRequest;
+import com.core.drm.crypto.repository.FileRequestRepository;
+import com.core.drm.crypto.util.FileUtil;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static com.core.drm.crypto.constant.errormessage.EntityExceptionMessage.NOT_FOUND_ID;
+
+@Service
+@RequiredArgsConstructor
+public class FileRequestService {
+
+    private final FileRequestRepository fileRequestRepository;
+
+    public void saveFileRequest(MultipartFile file, HttpServletRequest request, CipherType cipherType) {
+        String fileExtension = FileUtil.getFileExtension(file.getOriginalFilename());
+        FileRequest fileRequest = FileRequest.builder()
+                .fileName(file.getOriginalFilename())
+                .fileExtension(fileExtension)
+                .requestIP(request.getRemoteAddr())
+                .requestType(cipherType)
+                .requestTime(LocalDateTime.now())
+                .build();
+
+        fileRequestRepository.save(fileRequest);
+    }
+
+    public FileRequest findById(UUID requestId) {
+        String errMessage = String.format(NOT_FOUND_ID.getMessage(), FileRequest.class.getName(), requestId.toString());
+        return fileRequestRepository.findById(requestId)
+                .orElseThrow(() -> new EntityNotFoundException(errMessage));
+    }
+
+
+
+}
