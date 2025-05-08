6c29eee4a4bbd93cf35904cfdec343d1b217478f
jaewan <sjo9810@naver.com>
2025-05-08 14:33:58 +0900

    feat: 파일 요청 엔티티 생성

---
+++ src/main/java/com/core/drm/crypto/domain/entity/FileRequest.java
@@ -0,0 +1,38 @@
+package com.core.drm.crypto.domain.entity;
+
+import com.core.drm.crypto.constant.CipherType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "t_file_request")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileRequest {
+
+    @Id
+    @Column(name = "request_id")
+    private UUID requestId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "request_type")
+    private CipherType requestType;
+
+    @Column(nullable = false, name = "request_ip")
+    private String requestIP;
+
+    @Column(nullable = false, name = "file_name")
+    private String fileName;
+
+    @Column(nullable = false, name = "file_extension")
+    private String fileExtension;
+
+    @Column(nullable = false, name = "request_time")
+    private LocalDateTime requestTime;
+
+}
