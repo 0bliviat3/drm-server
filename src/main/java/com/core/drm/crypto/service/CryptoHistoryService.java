276690507719aa90c95ffb25f17f88b2f46e5595
jaewan <sjo9810@naver.com>
2025-05-09 17:56:56 +0900

    feat: 암호화 히스토리 레포지토리 구현

---
+++ src/main/java/com/core/drm/crypto/service/CryptoHistoryService.java
@@ -0,0 +1,30 @@
+package com.core.drm.crypto.service;
+
+import com.core.drm.crypto.constant.ProcessState;
+import com.core.drm.crypto.domain.entity.CryptoHistory;
+import com.core.drm.crypto.domain.entity.FileRequest;
+import com.core.drm.crypto.repository.CryptoHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CryptoHistoryService {
+
+    private final CryptoHistoryRepository cryptoHistoryRepository;
+
+    public CryptoHistory saveCryptoHistory(FileRequest fileRequest, ProcessState state) {
+        CryptoHistory cryptoHistory = CryptoHistory.builder()
+                .fileRequest(fileRequest)
+                .processState(state)
+                .processTime(LocalDateTime.now())
+                .build();
+
+        return cryptoHistoryRepository.save(cryptoHistory);
+    }
+
+}
