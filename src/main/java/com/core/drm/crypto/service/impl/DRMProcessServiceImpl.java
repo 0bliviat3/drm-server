bdf994a5dcf2cbe64882e26b89aea9cc9dc79d02
jaewan <sjo9810@naver.com>
2025-05-12 12:54:23 +0900

    refactor: 프로세스 히스토리 저장
    - 메서드 분리
    - 히스토리 저장 로직 추가

--- src/main/java/com/core/drm/crypto/service/impl/DRMProcessServiceImpl.java
+++ src/main/java/com/core/drm/crypto/service/impl/DRMProcessServiceImpl.java
@@ -1,6 +1,6 @@
 package com.core.drm.crypto.service.impl;
 
-import com.core.drm.crypto.constant.CipherType;
+import com.core.drm.crypto.constant.ProcessState;
 import com.core.drm.crypto.constant.errormessage.CipherExceptionMessage;
 import com.core.drm.crypto.domain.TempFile;
 import com.core.drm.crypto.domain.entity.FileRequest;
@@ -14,14 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.TriConsumer;
 import org.bouncycastle.crypto.BlockCipher;
 import org.bouncycastle.crypto.engines.AESLightEngine;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.swing.*;
 import java.io.*;
 
 import static com.core.drm.crypto.constant.CipherType.DECRYPT;
 import static com.core.drm.crypto.constant.CipherType.ENCRYPT;
+import static com.core.drm.crypto.constant.ProcessState.*;
 import static com.core.drm.crypto.constant.errormessage.CipherExceptionMessage.FAIL_DECRYPT;
 import static com.core.drm.crypto.constant.errormessage.CipherExceptionMessage.FAIL_ENCRYPT;
 
@@ -34,6 +35,7 @@ public class DRMProcessServiceImpl implements DRMProcessService {
     private final FileStorageService fileStorageService;
     private final FileRequestService requestService;
     private final FileTempStorageService fileTempStorageService;
+    private final CryptoHistoryService cryptoHistoryService;
 
 
     @Override
@@ -55,35 +57,53 @@ public class DRMProcessServiceImpl implements DRMProcessService {
         return FAIL_DECRYPT;
     }
 
-    public InputStream cryptProcess(
-            FileRequest fileRequest,
-            TriConsumer<InputStream, OutputStream, BlockCipher> triConsumer) {
-        boolean isEncrypt = fileRequest.isEncrypt();
+    private TempFile prepareTempFile(FileRequest fileRequest) {
         MultipartFile file = fileRequest.getFile();
         //파일 임시저장
         String savePath = FileUtil.saveTempFile(file, null);
         //임시저장파일 도메인 생성
-        TempFile tempFile = new TempFile(savePath);
+        TempFile tempFile =  new TempFile(savePath);
         fileTempStorageService.saveFileTempStorage(fileRequest, tempFile);
-        //임시파일 암복호화 정합성 검사
-        SignValidator.validateSign(tempFile, isEncrypt);
+        return tempFile;
+    }
+
+    private TempFile processFile(
+            FileRequest fileRequest,
+            TempFile tempFile,
+            TriConsumer<InputStream, OutputStream, BlockCipher> triConsumer) {
         //결과 출력 스트림 생성
         try (
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 InputStream inputStream = tempFile.getInputStream()
         ) {
-            //암호화처리
+            //암복호화처리
             triConsumer.accept(inputStream, outputStream, new AESLightEngine());
             byte[] outputByte = outputStream.toByteArray();
             //처리된 파일 임시저장
-            savePath = FileUtil.saveTempFile(outputByte, file.getOriginalFilename(), null);
+            String savePath = FileUtil.saveTempFile(outputByte, fileRequest.getFileName(), null);
+            TempFile cryptTempFile = new TempFile(savePath);
+            fileTempStorageService.saveFileTempStorage(fileRequest, cryptTempFile);
+            return cryptTempFile;
         } catch (IOException e) {
-            throw new CipherException(getErrorMsg(isEncrypt), e);
+            //프로세스 [실패]저장
+            cryptoHistoryService.saveCryptoHistory(fileRequest, FAIL);
+            throw new CipherException(getErrorMsg(fileRequest.isEncrypt()), e);
         }
-        //처리된 파일 임시파일 도메인으로 래핑
-        TempFile cipherFile = new TempFile(savePath);
-        fileTempStorageService.saveFileTempStorage(fileRequest, cipherFile);
+    }
 
+    private InputStream cryptProcess(
+            FileRequest fileRequest,
+            TriConsumer<InputStream, OutputStream, BlockCipher> triConsumer) {
+        //히스토리 [진행]저장
+        cryptoHistoryService.saveCryptoHistory(fileRequest, IN_PROCESS);
+        TempFile tempFile = prepareTempFile(fileRequest);
+        //임시파일 암복호화 정합성 검사
+        SignValidator.validateSign(tempFile, fileRequest.isEncrypt());
+        //처리된 파일 임시파일 도메인으로 래핑
+        TempFile cipherFile = processFile(fileRequest, tempFile, triConsumer);
+
+        //프로세스 [성공]저장
+        cryptoHistoryService.saveCryptoHistory(fileRequest, SUCCESS);
         return fileStorageService.responseFile(cipherFile, InputStream.class);
     }
 }
