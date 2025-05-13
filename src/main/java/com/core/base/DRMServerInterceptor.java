07bd21af51df3e97e0419e6a8c178fe8c692c57d
jaewan <sjo9810@naver.com>
2025-05-13 17:02:00 +0900

    feat: 인터셉터에서 응답처리할 서비스 추가

--- src/main/java/com/core/base/DRMServerInterceptor.java
+++ src/main/java/com/core/base/DRMServerInterceptor.java
@@ -1,19 +1,31 @@
 package com.core.base;
 
+import com.core.drm.crypto.service.ResponseProcessService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class DRMServerInterceptor implements HandlerInterceptor {
+
+    private final ResponseProcessService responseProcessService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("=====pre handle======");
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("=====post handle======");
+        responseProcessService.saveCryptoResponse(request.getRequestURI());
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 }
