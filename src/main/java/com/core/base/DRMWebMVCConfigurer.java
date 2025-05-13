394e8ae0514cc45edca6af2bdfafdb4ea753a93d
jaewan <sjo9810@naver.com>
2025-05-13 17:02:44 +0900

    feat: 인터셉터 추가방식 변경
    - 객체 생성방식보단 주입방식으로 변경함(스프링 빈으로 생성후 주입)

--- src/main/java/com/core/base/DRMWebMVCConfigurer.java
+++ src/main/java/com/core/base/DRMWebMVCConfigurer.java
@@ -1,15 +1,24 @@
 package com.core.base;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class DRMWebMVCConfigurer implements WebMvcConfigurer {
 
+    private final DRMServerInterceptor drmServerInterceptor;
+
+    public DRMWebMVCConfigurer(DRMServerInterceptor drmServerInterceptor) {
+        log.debug("load web config");
+        this.drmServerInterceptor = drmServerInterceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new DRMServerInterceptor())
+        registry.addInterceptor(drmServerInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/**", "/js/**", "/css/**");
     }
