ef517a3c353c3d313f8d8d46d3de9c16affb323e
jaewan <sjo9810@naver.com>
2025-05-13 16:59:31 +0900

    feat: 결과 조회 메서드 구현

--- src/main/java/com/core/drm/crypto/service/CryptoHistoryService.java
+++ src/main/java/com/core/drm/crypto/service/CryptoHistoryService.java
@@ -4,6 +4,7 @@ import com.core.drm.crypto.constant.ProcessState;
 import com.core.drm.crypto.domain.entity.CryptoHistory;
 import com.core.drm.crypto.domain.entity.FileRequest;
 import com.core.drm.crypto.repository.CryptoHistoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class CryptoHistoryService {
                 .build();
 
         return cryptoHistoryRepository.save(cryptoHistory);
+    }
+
+    public CryptoHistory findByRequestId(FileRequest fileRequest) {
+        return cryptoHistoryRepository
+                .findFirstByFileRequestOrderByProcessTimeDesc(fileRequest)
+                .orElseThrow(EntityNotFoundException::new);
     }
 
 }
