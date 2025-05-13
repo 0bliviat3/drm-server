681e68b853047bef6c168f878b2ac7b9a4632ec4
jaewan <sjo9810@naver.com>
2025-05-13 16:58:42 +0900

    feat: 히스토리 테이블에서 결과값 조회 기능 추가

--- src/main/java/com/core/drm/crypto/repository/CryptoHistoryRepository.java
+++ src/main/java/com/core/drm/crypto/repository/CryptoHistoryRepository.java
@@ -1,9 +1,12 @@
 package com.core.drm.crypto.repository;
 
 import com.core.drm.crypto.domain.entity.CryptoHistory;
+import com.core.drm.crypto.domain.entity.FileRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CryptoHistoryRepository extends JpaRepository<CryptoHistory, UUID> {
+    Optional<CryptoHistory> findFirstByFileRequestOrderByProcessTimeDesc(FileRequest fileRequest);
 }
