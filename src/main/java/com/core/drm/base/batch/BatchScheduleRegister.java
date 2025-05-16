d61dc14bb78aea631f7a83ce4d7f319cf492c3b4
jaewan <sjo9810@naver.com>
2025-05-16 17:37:24 +0900

    refactor: 스케줄서비스로추가
    - 서비스 로직 분리해서 리펙터링함

---
+++ src/main/java/com/core/drm/base/batch/BatchScheduleRegister.java
@@ -0,0 +1,25 @@
+package com.core.drm.base.batch;
+
+import com.core.drm.base.batch.domain.JobDefinition;
+import com.core.drm.base.batch.service.BatchSchedulerService;
+import com.core.drm.base.batch.service.JobDefinitionService;
+import lombok.RequiredArgsConstructor;
+import org.quartz.*;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class BatchScheduleRegister implements ApplicationRunner {
+
+    private final BatchSchedulerService batchSchedulerService;
+
+    @Override
+    public void run(ApplicationArguments args) throws SchedulerException {
+        batchSchedulerService.registryAllEnableJob();
+    }
+
+}
