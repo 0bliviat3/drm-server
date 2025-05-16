8a65fe23f566e0f736f731147b965f184a9d78bc
jaewan <sjo9810@naver.com>
2025-05-16 17:31:00 +0900

    refactor: 작업 상태 조회 쿼리 수정

--- src/main/java/com/core/drm/base/batch/repository/JobDefinitionRepository.java
+++ src/main/java/com/core/drm/base/batch/repository/JobDefinitionRepository.java
@@ -2,9 +2,20 @@ package com.core.drm.base.batch.repository;
 
 import com.core.drm.base.batch.domain.JobDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface JobDefinitionRepository extends JpaRepository<JobDefinition, String> {
-    List<JobDefinition> findByState(String state);
+
+    @Query("""
+            SELECT
+                *
+            FROM t_job_definition
+            WHERE 1 = 1
+            AND state = :state
+            AND data_code <> 'D'
+            """)
+    List<JobDefinition> findByState(@Param("state") String state);
 }
