5b7489b292aa415455b8a16c786c481a1d226852
jaewan <sjo9810@naver.com>
2025-05-16 17:35:53 +0900

    feat: 작업명세DTO 구현
    - toEntity 구현

---
+++ src/main/java/com/core/drm/base/batch/dto/JobDefinitionDTO.java
@@ -0,0 +1,27 @@
+package com.core.drm.base.batch.dto;
+
+import com.core.drm.base.util.MapToJsonConverter;
+import com.core.drm.base.batch.domain.JobDefinition;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Convert;
+
+import java.util.Map;
+
+public record JobDefinitionDTO(
+        String jobBeanName,
+        String state,
+        String cronExpression,
+
+        @Nullable String dataCode,
+        @Convert(converter = MapToJsonConverter.class) Map<String, String> jobParams
+) {
+    public JobDefinition toEntity() {
+        return JobDefinition.builder()
+                .jobBeanName(jobBeanName)
+                .cronExpression(cronExpression)
+                .state(state)
+                .dataCode(dataCode)
+                .jobParams(jobParams)
+                .build();
+    }
+}
