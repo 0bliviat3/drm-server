fef50193e0987b2f925c8d2631a025acbe3938a1
jaewan <sjo9810@naver.com>
2025-05-16 17:34:45 +0900

    feat: 임시 파일 삭제처리 프로세스서비스 구현

---
+++ src/main/java/com/core/drm/base/batch/service/TempFileDeleteService.java
@@ -0,0 +1,42 @@
+package com.core.drm.base.batch.service;
+
+import com.core.drm.crypto.domain.TempFile;
+import com.core.drm.crypto.domain.entity.FileTempStorage;
+import com.core.drm.crypto.service.CryptoResultService;
+import com.core.drm.crypto.service.FileTempStorageService;
+import com.core.drm.crypto.util.ListUtilsKt;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TempFileDeleteService {
+
+    private final CryptoResultService cryptoResultService;
+    private final FileTempStorageService fileTempStorageService;
+
+    public void fileDeleteProcess() {
+        List<FileTempStorage> tempFiles = fileTempStorageService.findTempFileListForDelete();
+        List<UUID> requestIds = new ArrayList<>();
+        for (FileTempStorage tempFile : tempFiles) {
+            new TempFile(tempFile.getFileSavePath()).delete();
+            requestIds.add(tempFile.getFileRequest().getRequestId());
+        }
+        resultUpdateFileStateToRemoved(requestIds);
+    }
+
+    private void resultUpdateFileStateToRemoved(List<UUID> requestIds) {
+        //TODO: 상수처리
+        List<List<UUID>> partitionIDs = ListUtilsKt.partition(requestIds, 1000);
+        for (List<UUID> list : partitionIDs) {
+            cryptoResultService.updateFileStateToRemoved(list);
+        }
+    }
+
+}
