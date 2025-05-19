99c4732ec6934ef65848a3bd864ce2af4e785995
jaewan <sjo9810@naver.com>
2025-05-19 17:47:50 +0900

    feat: 로그용 어노테이션 추가

--- src/main/java/com/core/drm/base/util/MapToJsonConverter.java
+++ src/main/java/com/core/drm/base/util/MapToJsonConverter.java
@@ -5,13 +5,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 @Converter(autoApply = false)
 public class MapToJsonConverter implements AttributeConverter<Map<String, String>, String> {
     @Override
     public String convertToDatabaseColumn(Map<String, String> stringStringMap) {
+
         try {
             return new ObjectMapper().writeValueAsString(stringStringMap);
         } catch (JsonProcessingException e) {
