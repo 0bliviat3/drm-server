ab5e9bc6e8f14d36616e026d4338ff6468fc88bc
jaewan <sjo9810@naver.com>
2025-05-19 17:49:11 +0900

    chore: 디버그용 로그 추가

--- src/main/java/com/core/drm/base/batch/service/TempFileDeleteService.java
+++ src/main/java/com/core/drm/base/batch/service/TempFileDeleteService.java
@@ -23,6 +23,7 @@ public class TempFileDeleteService {
 
     public void fileDeleteProcess() {
         List<FileTempStorage> tempFiles = fileTempStorageService.findTempFileListForDelete();
+        log.debug("임시파일목록 가져오기 성공!");
         List<UUID> requestIds = new ArrayList<>();
         for (FileTempStorage tempFile : tempFiles) {
             new TempFile(tempFile.getFileSavePath()).delete();
@@ -33,6 +34,7 @@ public class TempFileDeleteService {
 
     private void resultUpdateFileStateToRemoved(List<UUID> requestIds) {
         //TODO: 상수처리
+        log.debug("결과 테이블 업데이트 시작 ids: {}", requestIds.toString());
         List<List<UUID>> partitionIDs = ListUtilsKt.partition(requestIds, 1000);
         for (List<UUID> list : partitionIDs) {
             cryptoResultService.updateFileStateToRemoved(list);
