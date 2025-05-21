198640a163d880facb99c37158d0ed4ce775f917
jaewan <sjo9810@naver.com>
2025-05-21 15:06:34 +0900

    feat: 데이터 상태코드 반영한 계정 조회하기 추가

--- src/main/java/com/core/drm/admin/repository/UserRepository.java
+++ src/main/java/com/core/drm/admin/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.core.drm.admin.repository;
 
 import com.core.drm.admin.domain.User;
+import com.core.drm.base.constant.DataStateCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByIdAndDataCodeNot(String userId, DataStateCode dataStateCode);
 }
