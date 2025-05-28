dff1b7e3fabf4fcff6395a9a8479d93851c60714
jaewan <sjo9810@naver.com>
2025-05-28 16:05:40 +0900

    feat: 요청저장 로직 활성화
    - 요청자 데이터 수집로직 작성

--- src/main/java/com/core/drm/base/DRMServerInterceptor.java
+++ src/main/java/com/core/drm/base/DRMServerInterceptor.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -24,15 +25,13 @@ public class DRMServerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("=====pre handle======");
-        //saveRequest(request);
+        saveRequest(request);
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     private void saveRequest(HttpServletRequest request) {
-        //TODO: session helper 구현 후 userId 처리할것
-        //기본아이디: guest 생성해야함 (로그인시도는 guest로 기록)
-        //또는 예외처리 필요
-        String userId = null;
+        String userId = (String) Optional.ofNullable(request.getSession().getAttribute("userId"))
+                .orElse("guest"); //세션이 없다면 guest 처리
         String requestIP = request.getRemoteAddr();
         String requestURL = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
