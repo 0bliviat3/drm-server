9ab592fe052e5f94f31e370b20762df7ed508633
jaewan <sjo9810@naver.com>
2025-05-28 15:55:18 +0900

    fix: 사용자목록 페이징 처리 수정

--- src/main/java/com/core/drm/admin/service/SignService.java
+++ src/main/java/com/core/drm/admin/service/SignService.java
@@ -7,7 +7,6 @@ import com.core.drm.crypto.util.PropertiesUtil;
 import lombok.RequiredArgsConstructor;
 import org.postgresql.util.PasswordUtil;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -135,8 +134,7 @@ public class SignService {
                 .toDTOWithoutPassWord();
     }
 
-    public Page<UserDTO> findAll(int pageNumber, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+    public Page<UserDTO> findAll(Pageable pageable) {
         return userService.findAll(pageable).map(User::toDTOWithoutPassWord);
     }
 
